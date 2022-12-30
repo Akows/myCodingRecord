3a0155bb37b1be13e19d9654c5433b9dd0282425
AKOWSPC <akows141@gmail.com>
2022-12-30 11:15:16 +0900

    [기능 구현] [파이어베이스 로그인 / 데이터베이스] [코드 정리] - 구글 파이어베이스 로그인 기능 및 로그인 확인 기능 구현. - 에러 방지를 위해 경로상의 한글 명칭 및 숫자, 공백을 삭제 또는 수정함.

---
+++ CustomFunction/Firebase/loginanddatabase/src/Components/Home.js
@@ -0,0 +1,93 @@
+import './Home.css';
+
+import { useEffect, useState } from 'react';
+
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyDeDmKlMRVrir6S8le_7Iwc0RVNC9eYQ28',
+    authDomain: 'loginanddatabase-132d3.firebaseapp.com',
+    projectId: 'loginanddatabase-132d3',
+    storageBucket: 'loginanddatabase-132d3.appspot.com',
+    messagingSenderId: '713946772803',
+    appId: '1:713946772803:web:7faa48d1e6d7b01ba4abf6'
+};
+  
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+const Home = () => {
+
+    // admin@admin.com
+    // admin1234
+
+    const [isLogin, setIsLogin] = useState(false);
+
+    const [inputData, setInputData] = useState({
+        ID: '',
+        PWD: ''
+    });
+
+    const onChangeEvent = (event) => {
+        setInputData({
+            ...inputData,
+            [event.target.name] : event.target.value
+        })
+    };
+
+    const loginEvent = () => {
+        signInWithEmailAndPassword(auth, inputData.ID, inputData.PWD)
+          .then((userCredential) => {
+            const user = userCredential.user;
+            console.log(user);
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+          });
+    };  
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+          if (user) {
+            setIsLogin(user);
+          }
+          else {
+            setIsLogin(false);
+          }
+        });
+        console.log(auth);
+        console.log(isLogin);
+        // eslint-disable-next-line
+      }, []);
+
+    return (
+        <>
+            <div className='loginform'>
+
+                <h1>로그인</h1>
+
+                <div className='input'>
+                    <input name='ID' type='text' placeholder='아이디를 입력해주세요' value={inputData.ID} onChange={onChangeEvent}/>
+                </div>
+
+                <div className='input'>
+                    <input name='PWD' type='password' placeholder='비밀번호를 입력해주세요' value={inputData.PWD} onChange={onChangeEvent}/>
+                </div>
+
+                <div className='loginbutton' onClick={loginEvent}> 
+                    로그인 
+                </div>
+
+                <div onClick={loginEvent}> 
+                    로그인 확인
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Home;
